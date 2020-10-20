f94c0e434baba3c2e2f84b19f6da488ff6acacb0
Dev-Demi <BoxDemi@gmail.com>
2020-10-20 15:33:10 +0300

    Добавлены скрипты

---
+++ Блоки и сборки/Elastic.js
@@ -0,0 +1,15 @@
+const filename = 'С:\\f.fr3d'; // адрес до фрагмента
+var furn = OpenFurniture(filename);
+var obj = furn.Make(0, 0);
+obj.Owner = Model.Temp;
+if (obj && obj.List)
+{
+ obj.ElasticResize({
+  x: 1000,
+  y: 1500,
+  z: 400
+ })
+ obj.PositionX = 100;
+ obj.PositionY = 200;
+ obj.PositionZ = 300;
+}
