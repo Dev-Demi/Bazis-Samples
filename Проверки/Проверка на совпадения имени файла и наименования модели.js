d4a1f0aec685a1203ca38f180f4aa4d188f70df5
Dev-Demi <BoxDemi@gmail.com>
2021-03-30 15:15:27 +0300

    Create Проверка на совпадения имени файла и наименования модели.js
     * Проверяет идентичность написания имени файла
     * и наименования в параметрах изделия
     *
     * В случае различного написания, предлагает исправить написание модели
     *
     * Можно запускать при сохранении файла

---
+++ Проверки/Проверка на совпадения имени файла и наименования модели.js
@@ -0,0 +1,26 @@
+﻿/**
+ * Проверяет идентичность написания имени файла
+ * и наименования в параметрах изделия
+ *
+ * В случае различного написания, предлагает исправить написание модели
+ *
+ * Можно запускать при сохранении файла
+ */
+const pth = require("path");
+
+let filename = pth.basename(Action.ModelFilename).slice(0, -4);
+let name = Action.Control.Article.Name;
+
+if (filename != name && filename != "") {
+    let answer = confirm(
+        `Не совпадают имя файла и модели:
+            файл   - <${filename}>,
+            модель - <${name}>
+
+        Изменить наименование модели?`);
+    if (answer) {
+        Action.Control.Article.Name = filename;
+    }
+} else {
+    //Другие задачи
+}
